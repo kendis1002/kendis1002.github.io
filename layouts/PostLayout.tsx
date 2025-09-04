e97f60212e877f305a201c30a7355f22699b661b
kendis1002 <kendis1002@gmail.com>
2025-09-04 11:18:15 +0700

    update

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -68,9 +68,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       )}
                       <dl className="text-sm leading-5 font-medium whitespace-nowrap">
                         <dt className="sr-only">Name</dt>
-                        <dd className="left·text-gray-900·dark:text-gray-100">
-                          {author.name}
-                        </dd>
+                        <dd className="left·text-gray-900·dark:text-gray-100">{author.name}</dd>
                         <dt className="sr-only">Twitter</dt>
                         <dd>
                           {author.facebook && (
