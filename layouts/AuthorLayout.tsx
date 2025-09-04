663607544e8e95d03d107b87001938d550672187
kendis1002 <kendis1002@gmail.com>
2025-09-04 10:55:59 +0700

    update

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github } = content
+  const { name, avatar, occupation, company, email, facebook, bluesky, linkedin, github } = content
 
   return (
     <>
@@ -35,10 +35,7 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="x" href={twitter} />
-              <SocialIcon kind="bluesky" href={bluesky} />
+              <SocialIcon kind="facebook" href={facebook} />
             </div>
           </div>
           <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
